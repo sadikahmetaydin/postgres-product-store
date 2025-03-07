040b39dcd4ee7a8166afdc22fe0933353ce24e0c
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-08 02:00:00 +0300

    Database Setup, Connection PostgreSQL and Create Table

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import productRoutes from './routes/productRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -18,6 +19,27 @@ app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized successfully");
+
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
 });

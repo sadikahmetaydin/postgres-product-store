d8f7e6feec51583908b3a358c4c7d2b5f68ae3a3
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-09 00:24:19 +0300

    Rate Limiting and Bot Detection

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -16,6 +17,35 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1
+    })
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied "});
+      } else {
+        res.status(403).json({ error: "Forbidden:" });
+      }
+      return
+    }
+
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcject error", error);
+    next(error);
+  }
+})
 
 app.use("/api/products", productRoutes);
 

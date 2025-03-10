28b7951f01252afc2ccc01a449f9f218bdee99c6
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-10 20:19:10 +0300

    Delete Product Functionality

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,11 @@
 import { EditIcon, Trash2Icon } from "lucide-react"
 import { Link } from "react-router-dom"
+import { useProductStore } from "../store/useProductStore"
 
 function ProductCard({product}) {
+
+  const { deleteProduct } = useProductStore();
+
  return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
 
@@ -21,7 +25,7 @@ function ProductCard({product}) {
             <EditIcon className="size-4" />
           </Link>
 
-          <button className="btn btn-sm btn-error btn-outline">
+          <button className="btn btn-sm btn-error btn-outline" onClick={() => deleteProduct(product.id)}>
             <Trash2Icon className="size-4" />
           </button>
         </div>
--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const BASE_URL = "http://localhost:3000";
 
@@ -22,4 +23,19 @@ export const useProductStore = create((set, get) => ({
       set({ loading: false });
     }
   },
+
+  deleteProduct: async (id) => {
+    set({ loading: true });
+
+    try {
+      await axios.delete(`${BASE_URL}/api/products/${id}`);
+      set(prev => ({products: prev.products.filter(product => product.id !== id)}));
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      console.log("Error in deleteProduct function", error);
+      toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
+    }
+  }
 }));

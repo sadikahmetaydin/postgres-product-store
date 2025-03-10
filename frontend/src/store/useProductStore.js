702c4d7e74f854a3b4979aff1294c5da90f36251
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-03-10 21:04:48 +0300

    Add Product Functionality

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -10,6 +10,35 @@ export const useProductStore = create((set, get) => ({
   loading: false,
   error: null,
 
+  // form state
+  formData: {
+    name: "",
+    price: "",
+    image: "",
+  },
+
+  setFormData: (formData) => set({ formData}),
+  resetForm: () => set({ formData: {name: "", price: "", image: "" } }),
+
+  addProduct: async(e) => {
+    e.preventDefault();
+    set({ loading: true });
+
+    try {
+      const { formData } = get();
+      await axios.post(`${BASE_URL}/api/products`, formData);
+      await get().fetchProducts();
+      get().resetForm();
+      toast.success("Product added successfully");
+    } catch (error) {
+      console.log("Error in addProduct function", error);
+      toast.error("Something went wrong");
+      document.getElementById("add_product_modal").close();
+    } finally {
+      set({ loading: false });
+    }
+  },
+
   fetchProducts: async () => {
     set({ loading: true });
 
